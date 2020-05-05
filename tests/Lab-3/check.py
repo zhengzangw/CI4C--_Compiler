56f7725610ca46c8970986b030075c6b87aa6b7b
zhengzangw <zhengzangw@163.com>
2020-05-06 00:42:44 +0800

    update check

--- tests/Lab-3/check.py
+++ tests/Lab-3/check.py
@@ -7,15 +7,16 @@ def msg(s):
     print('\033[1m\033[91m' + s + '\033[0m\033[0m')
 
 
-def err(data_in, s):
-    msg("Wrong on input " + str(data_in) + "\n" + s)
+def err(data_in, data_out, s):
+    msg("Wrong on input " + str(data_in) + "\n" +
+        "Output mismatch, your output is " + str(s) + "\n" + "Output should be: " + str(data_out))
     exit(1)
 
 
 f_json = sys.argv[1]
 f_ir = sys.argv[2]
 program = "irsim/build/irsim"
-irsim_in  = 'CI4C--_Compiler/workdir/irsim_in'
+irsim_in = 'CI4C--_Compiler/workdir/irsim_in'
 irsim_out = 'CI4C--_Compiler/workdir/irsim_out'
 num_file = 'CI4C--_Compiler/workdir/count'
 
@@ -23,27 +24,35 @@ for data_in, data_out, ret_val in json.load(open(f_json)):
     with open(irsim_in, 'w+') as to_irsim_w:
         for i in data_in:
             to_irsim_w.write(str(i) + '\n')
-
-    ret = system("%s %s < %s > %s 2>/dev/null"%(program, f_ir , irsim_in , irsim_out))
-    # Suppose irsim is compiled by run.sh
+        # Run irsim
+    ret = system("%s %s < %s > %s 2>/dev/null" %
+                 (program, f_ir, irsim_in, irsim_out))
+    # Runtime error
     if ret != 0:
-        err(data_in, "Runtime error occured when running your IR code");
+        err(data_in, data_out, "Runtime error occured when running your IR code")
+        # Output mismatch
     with open(irsim_out, 'r') as from_irsim_r:
-        from_irsim_r.readline()
-        # Filter out the first line "load ./workdir/a.ir"
+        from_irsim_r.readline()  # Filter out the first line "load ./workdir/a.ir"
+        your_output = []
         try:
-            for i in data_out:
-                if i != int(from_irsim_r.readline()):
-                    err(data_in, "Output mismatch (output wrong number(s))")
-            else:
-                if "ret with 0, reason 0\n" != from_irsim_r.readline():
-                    err(data_in, "Output mismatch (output more number(s))")
-                cnt = int(from_irsim_r.readline())
-                with open(num_file, "r") as f:
-                    cnt = cnt + int(f.read())
-                with open(num_file, "w") as f:
-                    f.write(str(cnt))
+            temp_str = from_irsim_r.readline()
+            while temp_str[:8] != "ret with":
+                your_output.append(int(temp_str))
+                temp_str = from_irsim_r.readline()
+            if len(data_out) != len(your_output):
+                raise ValueError
+            for i in range(len(data_out)):
+                if data_out[i] != your_output[i]:
+                    raise ValueError
+            if temp_str != "ret with 0, reason 0\n":
+                raise ValueError
         except ValueError:
-            err(data_in, "Output mismatch (output less number(s))")
+            err(data_in, data_out, your_output)
+        else:
+            cnt = int(from_irsim_r.readline())
+            with open(num_file, "r") as f:
+                cnt = cnt + int(f.read())
+            with open(num_file, "w") as f:
+                f.write(str(cnt))
 
 exit(0)
